27a7e6cbe09ac522489f0d86f1f922316fdc2cf7
shahdivyam <divyamshah15@gmail.com>
2016-12-29 16:25:20 +0530

    merged with beacon

--- app/src/main/java/com/example/ayush/resturantninjas/MainActivity.java
+++ app/src/main/java/com/example/ayush/resturantninjas/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.ayush.resturantninjas;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,11 +23,33 @@ public class MainActivity extends AppCompatActivity implements RVAdapter.ClickLi
     Resturant resturant;
     Context context;
     public List<Resturant> resturants;
+    private static final int PERMISSIONS_REQUEST_CODE = 100 ;
+    private static String[] mPermissions = { Manifest.permission.ACCESS_FINE_LOCATION};
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    public Button mButton;
+    public TextView mTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         context=this;
+
+        mButton = (Button) findViewById(R.id.bt_1);
+        mTextView = (TextView) findViewById(R.id.tv_1);
+
+
+        if (!havePermissions()) {
+            Log.i("Permission", "Requesting permissions needed for this app.");
+            requestPermissions();
+        }
+
+        if(!isBlueEnable()){
+            Intent bluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(bluetoothIntent);
+        }
+
 
         RecyclerView rv = (RecyclerView)findViewById(R.id.rv);
         LinearLayoutManager llm = new LinearLayoutManager(this);
@@ -33,6 +63,21 @@ public class MainActivity extends AppCompatActivity implements RVAdapter.ClickLi
         RVAdapter adapter = new RVAdapter(this,resturants);
         adapter.setClickListner(this);
         rv.setAdapter(adapter);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            String str="";
+            @Override
+            public void onClick(View v) {
+                if (!MyApp.getInstance().regionList.isEmpty()){
+                    for(int i=0;i<MyApp.getInstance().regionList.size();i++){
+                        str+=MyApp.getInstance().regionNameList.get(i)+" , ";
+                    }
+                    mTextView.setText(str);
+
+                }
+            }
+
+        });
 
     }
 
@@ -61,5 +106,22 @@ public class MainActivity extends AppCompatActivity implements RVAdapter.ClickLi
 
         
 
+    }
+    private boolean isBlueEnable() {
+        BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+        return bluetoothAdapter.isEnabled();
+
+    }
+    private boolean havePermissions() {
+        for(String permission:mPermissions){
+            if(ActivityCompat.checkSelfPermission(this,permission)!= PackageManager.PERMISSION_GRANTED){
+                return  false;
+            }
+        }
+        return true;
+    }
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this,mPermissions, PERMISSIONS_REQUEST_CODE);
     }
 }
---
+++ app/src/main/java/com/example/ayush/resturantninjas/MyApp.java
@@ -0,0 +1,133 @@
+package com.example.ayush.resturantninjas;
+
+import android.app.Application;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.RemoteException;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Identifier;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by divyam on 12/29/16.
+ */
+public class MyApp extends Application implements BeaconConsumer {
+
+    private static MyApp instance = null;
+    private BeaconManager beaconManager;
+    private static final Identifier nameSpaceId = Identifier.parse("0x5dc33487f02e477d4058");
+
+    public ArrayList<String> regionNameList;
+    public ArrayList<Region> regionList;
+
+    public HashMap<String,Region> ssnRegionMap;
+
+
+
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.addMonitorNotifier(new MonitorNotifier() {
+            @Override
+            public void didEnterRegion(Region region) {
+
+            }
+
+            @Override
+            public void didExitRegion(Region region) {
+
+            }
+
+            @Override
+            public void didDetermineStateForRegion(int i, Region region) {
+                String regionName = region.getUniqueId();
+                String beaconSSN = region.getId2().toHexString();
+                if(i==INSIDE){
+                    Log.i("TAG","Inside"+regionName);
+                    regionNameList.add(regionName);
+                    regionList.add(region);
+
+                }
+                else if(i==OUTSIDE){
+                    Log.i("TAG","Outside"+regionName);
+                    if(regionList.contains(region)){
+                        regionList.remove(region);
+                    }
+                    if(regionNameList.contains(regionName)){
+                        regionNameList.remove(regionName);
+                    }
+                }
+
+            }
+        });
+
+        try {
+            for(String key:ssnRegionMap.keySet()) {
+                Region region = ssnRegionMap.get(key);
+                beaconManager.startMonitoringBeaconsInRegion(region);
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void showNotification(String message){
+        NotificationCompat.Builder mBuilder =   new NotificationCompat.Builder(instance)
+                .setSmallIcon(R.mipmap.ic_launcher) // notification icon
+                .setContentTitle("Welcome!") // title for notification
+                .setContentText(message) // message for notification
+                .setAutoCancel(true); // clear notification after click
+        NotificationManager mNotificationManager =
+                (NotificationManager) instance.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(0, mBuilder.build());
+
+    }
+
+    public static MyApp getInstance(){
+        return instance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+
+        setUpBeacon();
+    }
+
+    private void setUpBeacon() {
+
+        ssnRegionMap = new HashMap<>();
+        regionList = new ArrayList<>();
+        regionNameList = new ArrayList<>();
+
+        ssnRegionMap.put("0x0117c59825E9",new Region("Test Room",nameSpaceId, Identifier.parse("0x0117c59825E9"),null));
+        ssnRegionMap.put("0x0117c55be3a8",new Region("Git Room",nameSpaceId,Identifier.parse("0x0117c55be3a8"),null));
+        ssnRegionMap.put("0x0117c552c493",new Region("Android Room",nameSpaceId,Identifier.parse("0x0117c552c493"),null));
+        ssnRegionMap.put("0x0117c55fc452",new Region("iOS Room",nameSpaceId,Identifier.parse("0x0117c55fc452"),null));
+        ssnRegionMap.put("0x0117c555c65f",new Region("Python Room",nameSpaceId,Identifier.parse("0x0117c555c65f"),null));
+        ssnRegionMap.put("0x0117c55d6660",new Region("Office",nameSpaceId,Identifier.parse("0x0117c55d6660"),null));
+        ssnRegionMap.put("0x0117c55ec086",new Region("Ruby Room",nameSpaceId,Identifier.parse("0x0117c55ec086"),null));
+
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT));
+
+        new BackgroundPowerSaver(this);
+        beaconManager.bind(this);
+
+    }
+
+
+
+}
